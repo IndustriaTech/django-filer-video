c0c671fc9ba15e6668abc715aec6486afc219e22
Venelin Stoykov <venelin@magicbg.com>
2012-11-20 11:31:38 +0200

    Add setup for easy installing

---
+++ setup.py
@@ -0,0 +1,35 @@
+# encoding=utf8
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.md')
+
+setup(
+    name="django-filer-video",
+    version="0.0.1",
+    url='http://github.com/MagicSolutions/django-filer-video',
+    description="Extending django-filer and add suppor for Video Files",
+    long_description=README,
+
+    author='Venelin Stoykov',
+    author_email='venelin@magicbg.com',
+    packages=[
+        'filer_video',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        'django>=1.3',
+        'django-filer',
+    ],
+)
